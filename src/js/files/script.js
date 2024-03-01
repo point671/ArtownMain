0419aea820477c4e21fcbe1bc16d233535b9f52e
Point <sabakas3@mail.ru>
2024-03-01 17:56:18 +0400

    01

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,3 +4,13 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
+
+// const menuThreePoints = document.querySelector(".menu-hide-right-body-block-main-header-points")
+
+// menuThreePoints.addEventListener("click",addClassMenuThreePoints)
+
+
+// function addClassMenuThreePoints() {
+//     this.classList.toggle("menu-hide-right-body-block-main-header-points_active");
+// }
+
